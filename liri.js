e0c626672b978da31309ad2a844f72424e55e4e3
Maeve McQ <maevemcqueeny6@gmail.com>
2019-05-20 09:29:33 -0400

    Fixed the do it function, fixed the concertThis function

--- liri.js
+++ liri.js
@@ -7,14 +7,13 @@ var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require("axios");
-
+var moment = require("moment");
 // able to access your keys information like so
 var spotify = new Spotify(keys.spotify);
 
-//vars to capture user inputs.
+//vars to capture user process input and search terms.
 var action = process.argv[2];
 
-// Need to figure out how to do multiple words 
 var query = process.argv[3];
 
 // So many functions! This ones for you joe 
@@ -46,7 +45,7 @@ function Search(action, query) {
 
 // my little functions !
 
-
+// Spotify This
 function spotifyThis(query) {
     if (query === undefined) {
         query = "he mele no lilo";
@@ -66,12 +65,12 @@ function spotifyThis(query) {
             var songs = data.tracks.items;
 
             for (var i = 0; i < songs.length; i++) {
-                        // this will yield Arist, song name, a preview link of the song from Spotify, the album that the song is from
+                // this will yield Arist, song name, a preview link of the song from Spotify, the album that the song is from
 
-                console.log("the dirty deetz");
+                console.log("Your Search Results: ");
                 console.log("Song name " + songs[i].name);
                 console.log("Preview song " + songs[i].preview_url);
-                console.log("album: " + songs[i].album.name);
+                console.log("Album: " + songs[i].album.name);
                 console.log("Artists: " + songs[i].artists[0].name);
 
             }
@@ -79,61 +78,57 @@ function spotifyThis(query) {
     );
 };
 
-
-// var queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
-// request(queryURL, function (error){
-//     console.log(request);
-//     })
-
-// }
-
+// Movie This
 function movieThis(query) {
     if (query === undefined) {
         query = "Mr. Nobody";
+        // Who is so brazen in their movie reccomendations 
         console.log("If you haven't watched 'Mr. Nobody,' then you should: http://www.imdb.com/title/tt0485947/");
+        // Jared Leto is a dead ringer for Rasputin 
         console.log("It's on netflix");
     }
-    else{
-    var queryURL = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=b3c0b435";
+    else {
+        var queryURL = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=b3c0b435";
 
-            axios
-                .get(queryURL)
-                .then(function (response) {
-                    // If the axios was successful...
-                    // Then log the body from the site!
-                    console.log("Movie Title: " + response.data.Title);
-                    console.log("Year Released: " + response.data.Year);
-                    console.log("IMDB Rating: " + response.data.Ratings[0].Value);
-                    console.log("Rotten Tomatoes Critic Score: " + response.data.Ratings[1].Value);
-                    console.log("Country Produced In: " + response.data.Country);
-                    console.log("Language(s): " + response.data.Language);
-                    console.log("Plot Summary: " + response.data.Plot);
-                    console.log("Cast: " + response.data.Actors);        
-                })
-        }
+        axios
+            .get(queryURL)
+            .then(function (response) {
+                // If the axios was successful...
+                // Then log the body from the site!
+                console.log("Movie Title: " + response.data.Title);
+                console.log("Year Released: " + response.data.Year);
+                console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                console.log("Rotten Tomatoes Critic Score: " + response.data.Ratings[1].Value);
+                console.log("Country Produced In: " + response.data.Country);
+                console.log("Language(s): " + response.data.Language);
+                console.log("Plot Summary: " + response.data.Plot);
+                console.log("Cast: " + response.data.Actors);
+            })
     }
+};
 
+// Concert This
+function concertThis(query) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+    axios.get(queryURL).then(function (response) {
+        // var to format w/ Moment JS
+        // console.log(response.data[0])
+        var date = moment(response.data[0].datetime).format('MM/DD/YYYY')
+        console.log("Go see " + response.data[0].lineup[0] + " at " + response.data[0].venue.name + " in " + response.data[0].venue.city + " on " + date + "!");
+    })
+        .catch(function (error) {
+            console.log(error);
+        });
+}
 
-        // concert-this should yield <artist / band name here>; this will search the Bands in Town Artist Event API 
-        // This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal: 1 Name of the venue 2 Venue location 3 Date of the Event (use moment to format this as "MM/DD/YYYY")
+function justDoIt() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
 
-
-
-        // spotify-this-song should yield <song namehere>
-        // this will yield Arist, song name, a preview link of the song from Spotify, the album that the song is from
-
-        // if no song is provided then your program will default to 'the sign' by ace of base
-
-
-
-        // movie-this <movie name here > ; this will output title, year the movie came out, IMDB Rating, Rotten tomatoes rating, country movie was produced, language of movie, plot of movie, actors in move
-
-
-        // if the user doesn't type a movie in, the program will suggest Mr. Nobody
-        // Use axious package to retrieve data from OMDB
-
-
-
-        // do-what-it-says will take text inside random.txt and then use it to call one of liris commands 
-        // use fs node package 
-
+        // Look Over
+        var input = data.split(',');
+        UserInputs(input[0], input[1]);
+    });
+};
